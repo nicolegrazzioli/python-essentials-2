ea317754d47c04b407e49f211e6153bff0f9b711
Nicole Grazzioli <nicolegrazzioli@gmail.com>
2025-12-08 20:19:24 -0300

    sum of life

---
+++ activities/lab-digit-of-life.py
@@ -0,0 +1,34 @@
+"""Some say that the Digit of Life is a digit evaluated using somebody's birthday. It's simple – you just need to sum all the digits of the date. If the result contains more than one digit, you have to repeat the addition until you get exactly one digit. For example:
+
+1 January 2017 = 2017 01 01
+2 + 0 + 1 + 7 + 0 + 1 + 0 + 1 = 12
+1 + 2 = 3
+3 is the digit we searched for and found.
+
+Your task is to write a program which:
+
+asks the user her/his birthday (in the format YYYYMMDD, or YYYYDDMM, or MMDDYYYY – actually, the order of the digits doesn't matter)
+outputs the Digit of Life for the date.
+Test your code using the data we've provided.
+
+Test Data
+Sample input:
+
+19991229
+
+Sample output:
+
+6"""
+
+date = input("Enter your birthday (any format): ")
+
+datef = ' '.join(date).split()
+
+datef = list(map(int, datef))
+
+
+total = sum(datef)
+while total > 9:
+    total = sum(map(int, str(total)))
+
+print(total)
