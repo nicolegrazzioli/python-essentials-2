63c90b0231d345b1eb20214e1e0c9bbe25999eab
Nicole Grazzioli <nicolegrazzioli@gmail.com>
2025-12-08 21:46:11 -0300

    sudoku linha e coluna

---
+++ activities/lab-sudoku.py
@@ -0,0 +1,65 @@
+"""As you probably know, Sudoku is a number-placing puzzle played on a 9x9 board. The player has to fill the board in a very specific way:
+
+each row of the board must contain all digits from 0 to 9 (the order doesn't matter)
+each column of the board must contain all digits from 0 to 9 (again, the order doesn't matter)
+each of the nine 3x3 "tiles" (we will name them "sub-squares") of the table must contain all digits from 0 to 9.
+If you need more details, you can find them here.
+
+Your task is to write a program which:
+
+reads 9 rows of the Sudoku, each containing 9 digits (check carefully if the data entered are valid)
+outputs Yes if the Sudoku is valid, and No otherwise.
+Test your code using the data we've provided.
+
+Test Data
+Sample input:
+
+295743861
+431865927
+876192543
+387459216
+612387495
+549216738
+763524189
+928671354
+154938672
+
+Sample output:
+
+Yes"""
+
+size = 9
+
+text = input("Enter Sudoku rows: ").replace('\n', '')
+if len(text) != size * size or not text.isdigit() or '0' in text:
+    print("No")
+    exit()
+
+sudoku = []
+
+for i in range(size):
+    row = []
+    for j in range(size):
+        row.append(int(text[i * size + j]))
+    sudoku.append(row)
+
+# linhas ok
+for row in sudoku:
+    copy = sorted(row)
+    print(f"linha = {row}\tordenado = {copy}")
+    if copy != list(range(1, 10)):
+        print("No")
+        exit()
+        
+# colunas ok
+for j in range(size):
+    col = []
+    for i in range(size):
+        col.append(sudoku[i][j]) #extrai coluna
+    copy2 = sorted(col)
+    print(f"coluna = {col}\tordenado = {copy2}")
+    if copy2 != list(range(1, 10)):
+        print("No")
+        exit()
+
+# 3x3
