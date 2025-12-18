07d60b4abd9c7d404f6a781c003fabe555be0b33
Nicole Grazzioli <nicolegrazzioli@gmail.com>
2025-12-17 22:10:56 -0300

    atts

--- draft.py
+++ draft.py
@@ -1,91 +1,21 @@
-class Classy:
-    varia = 2
-    def method(self):
-        print(self.varia, self.var)
-        self.other()
+def reciprocal(n):
+    try:
+        n = 1 / n
+    except ZeroDivisionError:
+        print("Division failed")
+        n = None
+    else:
+        print("Everything went fine")
+    finally:
+        print("It's time to say goodbye")
+        return n
 
-    def other(self):
-        print("other")
-  
-obj = Classy()
-obj.var = 3
-obj.method()
+print(reciprocal(2))
+print(reciprocal(0))
 
-# 2 3 
-# other
-
-######################
-class Classy:
-    def visible(self):
-        print("visible")
-
-    def __hidden(self):
-        print("hidden")
-
-obj = Classy()
-obj.visible() # visible
-
-try:
-    obj.__hidden()
-except:
-    print("failed") # failed
-
-obj._Classy__hidden() # hidden
-
-(obj.__module__)
-
-#################
-class SuperOne:
-    pass
-
-class SuperTwo:
-    pass
-
-class Sub(SuperOne, SuperTwo):
-    pass
-
-def printBases(cls):
-    print('( ', end='')
-    for x in cls.__bases__:
-        print(x.__name__, end=' ')
-    print(')')
-
-printBases(SuperOne) # ( object )
-printBases(SuperTwo) # ( object )
-printBases(Sub) # ( SuperOne SuperTwo )
-
-#####################
-class MyClass:
-    pass
-
-obj = MyClass()
-obj.a = 1
-obj.b = 2
-obj.i = 3
-obj.ireal = 3.5
-obj.integer = 4
-obj.z = 5
-
-def incIntsI(obj):
-    for name in obj.__dict__.keys(): #busca nomes dos atributos
-        if name.startswith('i'):
-            val = getattr(obj, name) #pega valor atual
-            if isinstance(val, int): #verifica se é do tipo integer
-                setattr(obj, name, val + 1) #(objeto, atributo, novo valor)
-
-print(obj.__dict__)
-# {'a': 1, 'integer': 4, 'b': 2, 'i': 3, 'z': 5, 'ireal': 3.5}
-incIntsI(obj)
-print(obj.__dict__)
-# {'a': 1, 'integer': 5, 'b': 2, 'i': 4, 'z': 5, 'ireal': 3.5}
-
-##########################
-class Sample:
-    def __init__(self):
-        self.name = Sample.__name__
-    def myself(self):
-        print("My name is " + self.name + " living in a " + Sample.__module__)
-
-obj = Sample()
-obj.myself()
-# My name is Sample living in a __main__
+# Everything went fine
+# It's time to say goodbye
+# 0.5
+# Division failed
+# It's time to say goodbye
+# None
