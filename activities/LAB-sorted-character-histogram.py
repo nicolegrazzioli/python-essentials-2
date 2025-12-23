fa25c685b9374a10140efb9005b7fc0b4fdcf430
Nicole Grazzioli <nicolegrazzioli@gmail.com>
2025-12-22 22:30:09 -0300

    histograma ordenado por frequencia

---
+++ activities/LAB-sorted-character-histogram.py
@@ -0,0 +1,30 @@
+from os import strerror
+import string, unicodedata
+
+# counts = {letra: 0 for indice, letra in enumerate(letras)}
+counts = {}
+
+f = input("name of the file (files/histogram.txt): ")
+
+try:
+    file = open(f, 'rt')
+    stream = file.read()
+
+    stream = unicodedata.normalize('NFD', stream) # separa acentos etc
+
+    for c in stream:
+        c = c.lower()
+
+        if c in string.ascii_lowercase:
+            if c not in counts:
+                counts[c] = 1
+            else:
+                counts[c] += 1
+
+    # sorted_counts = dict(sorted(counts.items()))
+
+    for key in sorted(counts, key=counts.get, reverse=True):
+        print(key, "->", counts[key])
+
+except IOError as e:
+    print("error: ", strerror(e.errno))
