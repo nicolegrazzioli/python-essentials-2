fff3a11c6f496bde22c10138dc6e113a9d545854
Nicole Grazzioli <nicolegrazzioli@gmail.com>
2025-12-22 22:43:41 -0300

    sorted histogram to new file

--- activities/LAB-sorted-character-histogram.py
+++ activities/LAB-sorted-character-histogram.py
@@ -1,7 +1,24 @@
+"""The previous code needs to be improved. It's okay, but it has to be better.
+
+Your task is to make some amendments, which generate the following results:
+
+the output histogram will be sorted based on the characters' frequency (the bigger counter should be presented first)
+the histogram should be sent to a file with the same name as the input one, but with the suffix '.hist' (it should be concatenated to the original name)
+Assuming that the input file contains just one line filled with:
+
+cBabAa
+samplefile.txt
+the expected output should look as follows:
+
+Output
+a -> 3
+b -> 2
+c -> 1"""
+
 from os import strerror
 import string, unicodedata
 
-# counts = {letra: 0 for indice, letra in enumerate(letras)}
+# new = {letra: 0 for indice, letra in enumerate(letras)}
 counts = {}
 
 f = input("name of the file (files/histogram.txt): ")
@@ -21,10 +38,17 @@ try:
             else:
                 counts[c] += 1
 
-    # sorted_counts = dict(sorted(counts.items()))
+    new = f + '.hist'
+    try:
+        with open(new, 'wt') as out:
+            for key in sorted(counts, key=counts.get, reverse=True):
+                # print(key, "->", new[key])
+                linha = f"{key} -> {counts[key]}\n"
+                out.write(linha)
+            print("file saved: ", new)
 
-    for key in sorted(counts, key=counts.get, reverse=True):
-        print(key, "->", counts[key])
+    except IOError as e:
+        print("error: ", strerror(e.errno))
 
 except IOError as e:
     print("error: ", strerror(e.errno))
