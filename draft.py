da4b42c2190253e3d603a89bcac1643cb4bd2bde
Nicole Grazzioli <nicolegrazzioli@gmail.com>
2025-12-28 15:13:55 -0300

    finished aleluia

---
+++ draft.py
@@ -0,0 +1,7 @@
+# 6 82 0
+
+randrange(begin, end, step)
+rangeint(esq, dir) – números inteiros, limite incluso = randrange(left, right+1)
+choice(sequence) – pega um elemento “aleatório” de uma lista e retorna
+
+C 
